3685384c87d93e413b15b0f151e0e0dfb40f2b1c
Crowdbotics <team@crowdbotics.com>
2024-03-14 03:41:54 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solitary-term-47034",
+  url: "https://solitary-term-47034.botics.co",
+  api: "https://solitary-term-47034.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
